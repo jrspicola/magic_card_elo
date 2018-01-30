ace559f46b1b347ea4b6520d068d8c9b63d8d1d2
JSpicola <joespicola@gmail.com>
2018-01-30 01:23:39 -0500

    added the card ranking model and updated the admin page for card/comparison/result

--- magic_elo_site/mtg_compare/admin.py
+++ magic_elo_site/mtg_compare/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import CardSet, CardColor, CardType, Card, CardComparison, CardComparisonResult, CardRanking
 
-# Register your models here.
+admin.site.register(CardSet)
+admin.site.register(CardColor)
+admin.site.register(CardType)
+admin.site.register(CardRanking)
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+	list_display = ('name', 'card_color_display', 'card_type_display', 'cmc')
+	list_filter = ('cardColor', 'cardType', 'cardSet')
+
+@admin.register(CardComparison)
+class CardComparisonAdmin(admin.ModelAdmin):
+	list_display = ('id', 'leftCard', 'rightCard')
+
+@admin.register(CardComparisonResult)
+class CardComparisonResultAdmin(admin.ModelAdmin):
+	list_display = ('id', 'winner', 'loser')
--- magic_elo_site/mtg_compare/models.py
+++ magic_elo_site/mtg_compare/models.py
@@ -3,35 +3,34 @@ from django.db import models
 ELO_DEFAULT = 1400
 
 class CardColor(models.Model):
-	cardColor = models.CharField(max_length=20, help_text="Enter a color")
+	cardColor = models.CharField(max_length=20, help_text="Enter a color", unique=True)
 
 	def __str__(self):
-		return self.name
+		return self.cardColor
 
 class CardType(models.Model):
-	cardType = models.CharField(max_length=50, help_text="Enter a card type")
+	cardType = models.CharField(max_length=50, help_text="Enter a card type", unique=True)
 
 	def __str__(self):
-		return self.name
+		return self.cardType
 
 class CardSet(models.Model):
-	cardSet = models.CharField(max_length=50, help_text="Enter the name of a set")
+	cardSet = models.CharField(max_length=50, help_text="Enter the name of a set", unique=True)
 
 	def __str__(self):
-		return self.name
+		return self.cardSet
 
 class Card(models.Model):
 	'''
 	Model representing a single Magic Card.
 	'''
-	name = models.CharField(max_length=200)
+	name = models.CharField(max_length=200, unique=True)
 	cmc = models.IntegerField(default=0, help_text="Enter the converted mana cost of this card")
 
 	cardType = models.ManyToManyField(CardType, help_text="Select a type for this card")
 	cardColor = models.ManyToManyField(CardColor, help_text="Select a color for this card")
 	cardSet = models.ManyToManyField(CardSet, help_text="Select a set this card is from")
 
-	elo = models.IntegerField(default=ELO_DEFAULT, help_text="Enter the starting Elo for this card")
 
 	def __str__(self):
 		return self.name
@@ -39,6 +38,14 @@ class Card(models.Model):
 	def get_absolute_url(self):
 		'''returns the url to access a particular card information'''
 		return reverse('card-detail', args=[str(self.id)])
+
+	def card_type_display(self):
+		return ', '.join([ cardType.cardType for cardType in self.cardType.all()])
+	card_type_display.short_description = 'Type'
+
+	def card_color_display(self):
+		return ', '.join([ cardColor.cardColor for cardColor in self.cardColor.all()])
+	card_color_display.short_description = 'Color'
 
 class CardComparison(models.Model):
 	'''
@@ -48,19 +55,28 @@ class CardComparison(models.Model):
 	rightCard = models.ForeignKey(Card, on_delete=models.CASCADE, related_name="right_card")
 
 	def __str__(self):
-		return self.name
+		return str(self.id) + ': ' + str(self.leftCard) + ' vs. ' + str(self.rightCard)
 
 class CardComparisonResult(models.Model):
 	'''
 	Model representing a completed comparison
 	'''
+	comparison = models.ForeignKey(CardComparison, on_delete=models.CASCADE)
 	winner = models.ForeignKey(Card, on_delete=models.CASCADE, related_name="winning_card")
 	loser = models.ForeignKey(Card, on_delete=models.CASCADE, related_name="losing_card")
 
-	comparison = models.ForeignKey(CardComparison, on_delete=models.CASCADE)
-
 	def __str__(self):
-		return self.name
+		return str(self.id) + ': ' + str(self.winner) + ' DEFEATED ' + str(self.loser)
 
 	def get_absolute_url(self):
 		return reverse('card-comparison-result-detail', args=[str(self.id)])
+
+class CardRanking(models.Model):
+	'''
+	Model representing an Elo ranking for a specific card
+	'''
+	card = models.OneToOneField(Card, on_delete=models.CASCADE)
+	elo = models.IntegerField(default=ELO_DEFAULT, help_text="Enter the starting Elo for this card")
+
+	def __str__(self):
+		return self.card.name
